fbc92d3af4cf23a65743c5a15ffc04e7eae36d18
Daniel Morris <danny.morris280@gmail.com>
2013-06-25 13:11:19 -0600

    Initial commit workflow editor

---
+++ Ame.WorkflowHTML5/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ame.WorkflowHTML5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult PreRelationalList()
+        {
+            return View();
+        }
+
+        public ActionResult RelationalMap()
+        {
+            return View();
+        }
+
+        public ActionResult SwimlaneChart()
+        {
+            return View();
+        }
+
+        public ActionResult SwimlaneMap123()
+        {
+            return View();
+        }
+
+        public ActionResult Flowchart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ame.WorkflowHTML5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ame.WorkflowHTML5.MvcApplication" Language="C#" %>
