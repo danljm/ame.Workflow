adbc73689e8e665f53ca7751b0d6b17e72407c17
Daniel Morris <danny.morris280@gmail.com>
2013-07-10 13:33:53 -0600

    softening colors for palette and draw canvas add javascript helper

---
+++ Ame.WorkflowHTML5/Scripts/jsPlumb-save-functions.js
@@ -0,0 +1,51 @@
+﻿//// functions ////
+
+// repaint
+function Repaint() {
+    $("#main").resize(function () {
+        jsPlumb.repaintEverything();
+    });
+}
+// размер
+function ResizeEl(el) {
+    $(el).resizable();
+}
+// drag
+function DragEl(el) {
+    jsPlumb.draggable($(el));
+}
+
+// save
+function Save() {
+    //$(".shape").resizable("destroy");
+
+    chart = $("#pallette-draw").html();
+    Objs = [];
+    $(".shape").not(".pallette").each(function () {
+        Objs.push({ id: $(this).attr('id'), html: $(this).html(), left: $(this).css('left'), top: $(this).css('top'), width: $(this).css('width'), height: $(this).css('height') });
+    });
+    console.log(chart);
+}
+
+// load
+function Load() {
+    var s = "";
+    for (var i in Objs) {
+        var o = Objs[i];
+        console.log(o);
+        s += '<div id="' + o.id + '" class="shape rectangle" style="left:' + o.left + '; top:' + o.top + '; width:' + o.width + '; height:' + o.height + ' "> ' + o.html + '</div>';
+    }
+    $('#pallette-draw').html(chart);
+
+    /** Externalize into a function to be shared */
+    var shapes = $(".shape");
+
+    jsPlumbDemo.init();
+}
+
+// clear
+function Clear() {
+    jsPlumb.detachEveryConnection();
+    jsPlumb.reset();
+    $("#pallette-draw").empty();
+}
