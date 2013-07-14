08e23134511c4e9d44390c5b3b20bccada92a6fd
Daniel Morris <danny.morris280@gmail.com>
2013-07-14 17:05:10 -0600

    styling for shapes and connectors

---
+++ Ame.WorkflowHTML5/Repositories/ChartRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ame.WorkflowHTML5.Models;
+
+namespace Ame.WorkflowHTML5.Repositories
+{
+    public class ChartRepository
+    {
+
+        public void Initialize()
+        {
+            List<Chart> charts = new List<Chart>();
+
+            HttpContext.Current.Session["Students"] = charts;
+        }
+
+        public IList<Chart> GetCharts()
+        {
+            return (List<Chart>)
+                HttpContext.Current.Session["Charts"];
+        }
+
+        public void SetCharts(IList<Chart> charts)
+        {
+            HttpContext.Current.Session["Charts"] = charts;
+        }
+
+    }
+}
